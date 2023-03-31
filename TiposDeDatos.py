51690a49432551e9176b6b09df73cd8d05df9d7a
Juan Esteban Parra Parra <juane.parrap@uqvirtual.edu.co>
2023-03-31 13:56:10 -0500

    Leccion tipos de datos

---
+++ TiposDeDatos.py
@@ -0,0 +1,18 @@
+#Strings --> Palabras
+palabra = "Valery"
+
+#Numeros --> Enteros y flotantes.
+myEntero = 10
+myFlotante = 10.5
+
+
+#booleano ---> Solo dos valores
+estaSoleado = True
+dos_mas_igual_cinco = True
+
+
+print("El día está soleado?")
+print(estaSoleado)
+print("Dos más dos es igual a cinco?")
+print(dos_mas_igual_cinco)
+print(suma_de_Bools)
